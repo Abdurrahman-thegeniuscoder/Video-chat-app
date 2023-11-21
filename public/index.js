1271a9e2222a4b29eb228543433846e81750fb71
Abdurrahman-thegeniuscoder <abdurrahmanbabayemi@gmail.com>
2023-11-21 12:55:28 +0100

    Added the connection of multiple peers

--- public/index.js
+++ public/index.js
@@ -1,44 +1,89 @@
-const socket = io("/")
-const videoGrid = document.getElementById("video-grid")
-const myVideo = document.createElement("video")
-myVideo.muted = true
-var peer = new Peer()
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
-        videoGrid.append(video);
-        video.play();
-     });
-}
+const user = prompt("Enter your name");
+
+const peer = new Peer({
+  host: '127.0.0.1',
+  port: 3030,
+  path: '/peerjs',
+  config: {
+    'iceServers': [
+      { url: 'stun:stun01.sipphone.com' },
+      { url: 'stun:stun.ekiga.net' },
+      { url: 'stun:stunserver.org' },
+      { url: 'stun:stun.softjoys.com' },
+      { url: 'stun:stun.voiparound.com' },
+      { url: 'stun:stun.voipbuster.com' },
+      { url: 'stun:stun.voipstunt.com' },
+      { url: 'stun:stun.voxgratia.org' },
+      { url: 'stun:stun.xten.com' },
+      {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      },
+      {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      }
+    ]
+  },
+  debug: 3
+});
+
+let myVideoStream;
+
+const getMediaStream = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      console.log('someone call me');
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  } catch (error) {
+    console.error("Error accessing media devices:", error);
+  }
+};
+
+getMediaStream();
 
 const connectToNewUser = (userId, stream) => {
-    const call = peer.call(userId, stream)
-    const video = document.createElement("video")
-    call.on("stream", () => {
-        addVideoStream(video, userVideoStream)
-    })
-}
+  console.log('I call someone' + userId);
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
-// socket.emit("join-room", ROOM_ID, 10)
+peer.on("open", (id) => {
+  console.log('my id is' + id);
+  socket.emit("join-room", ROOM_ID, id, user);
+});
 
-async function startVideo() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true, 
-        video: true,
-    })
-    addVideoStream(myVideo, stream)
-    
-    peer.on("call", (call) => {
-        call.answer(stream)
-        const video = document.createElement("video");
-        call.on("stream", (userVideoStream) => {
-            addVideoStream(video, userVideoStream)
-        })
-    })
-    socket.on("user-connected", (userId) => {
-        connectToNewUser(userId, stream)
-    })
-}
-
-startVideo()
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    videoGrid.append(video);
+  });
+};
--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 import express from "express";
 import { v4 as uuidv4 } from 'uuid';
-import { createServer } from "https";
+import { createServer } from "http";
 import { Server } from "socket.io";
+import { ExpressPeerServer } from "peer";
 
 const app = express()
 const server = createServer(app)
 const port = 3000
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+      origin: '*',
+    }
+});
 
+const opinions = {
+    debug: true,
+}
+app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`)
 })
 
 app.get("/:room", (req, res) => {
-    res.render("room.ejs", {roomId: req.params.room})
+    res.render("room", {roomId: req.params.room})
 })
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId) => {
-        console.log(roomId, userId)
+        // console.log(roomId, userId)
         socket.join(roomId)
-        socket.to(roomId).emit("user-connected", userId)
+        setTimeout(()=>{ 
+            socket.to(roomId).emit("user-connected", userId);
+          }, 100)
     })
-})
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+}) 
+
+server.listen(process.env.PORT || 3000);
